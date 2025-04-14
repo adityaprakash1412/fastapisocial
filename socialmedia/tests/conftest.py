020d0a037ca3a42b41d6369bd0707ef4ad471a58
adityaprakash1412 <adityaprakash27997@gmail.com>
2025-04-14 22:17:45 +0530

    new changes

--- socialmedia/tests/conftest.py
+++ socialmedia/tests/conftest.py
@@ -7,3 +7,22 @@ from httpx import AsyncClient
 from socialmedia.main import app
 from socialmedia.routers.post import comment_table, post_table
 
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator: #Dependency Injection
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
+
